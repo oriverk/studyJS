aa229d377f11836f61021d90199794e5f4368392
oriverk <se.sterroristalqaeda@gmail.com>
2020-05-31 23:34:18 +0900

    change directory structure

--- README.md
+++
@@ -1,2 +0,0 @@
-# README
-This repositry is for study JavaScript mainly with Dotinstall.
--- tmep/favo.js
+++
@@ -1,6 +0,0 @@
-{
-  'use strict';
-
-  var mainContents = document.querySelectorAll('div.js-app-columns.app-columns');
-  var 
-}
